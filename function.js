75c952bbae96ad98cb8226148361c4bbaa1d49ff
toha245 <mdabdulawal2001@gmail.com>
2025-10-14 23:30:12 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,31 @@
+const price = 45;
+
+// function declare
+function fanOffKor(){
+    console.log('bosa theke uthe dara');
+    console.log('walk towards switch');
+    console.log('click the switch to off of the Fan');
+}
+
+// call the function
+fanOffKor();
+
+// function name(){
+//
+// }
+function brushTeeth(){
+    console.log('pick up the brush');
+    console.log('add paste');
+    console.log('2 min goso and goso and fena felo');
+}
+
+// call the function
+brushTeeth();
+console.log('--------------------');
+console.log('-------------------');
+console.log('-------------------');
+console.log('tom tom ta to');
+
+brushTeeth();
+
+// console.log();
