c5c7ceb2740c6181806fe9856dda81ce5de24ccc
toha245 <mdabdulawal2001@gmail.com>
2025-10-19 16:19:37 +0600

    second commit

---
+++ return.js
@@ -0,0 +1,18 @@
+function tenTimes (number){
+    const result = number * 10;
+    return result;
+    // console.log(result); return er moddhei code er kaj ses.
+}
+
+function cutHalf (number){
+    const half = number / 2;
+    return half;
+}
+
+tenTimes(5);
+
+const output = tenTimes(5);
+console.log('output', output);
+
+const bigNumber = tenTimes(99);
+console.log(bigNumber);
