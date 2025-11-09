5648c762e6d3becc95af7de7e7cdc8451e4c2377
toha245 <mdabdulawal2001@gmail.com>
2025-11-09 23:24:24 +0600

    last commit

--- module_21_practice_tasks.js
+++ module_21_practice_tasks.js
@@ -58,17 +58,16 @@
 
 // task 5 : Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
 
-function odd_even(number) {
-    if(number % 2 === 0){
-        return 'Even';
-    }
-    else{
-        return 'Odd';
-    }
-}
-
-const result = odd_even(12);
-console.log(result);
+// function odd_even(number) {
+//     if(number % 2 === 0){
+//         return 'Even';
+//     }
+//     else{
+//         return 'Odd'; // return number % 2 === 0 ? "Even" : "Odd";
+//     }
+// }
+// const result = odd_even(12);
+// console.log(result);
 
 
 
