5319457dcf706112bfd8089ccb78d96b2d17bce0
toha245 <mdabdulawal2001@gmail.com>
2025-11-09 19:42:29 +0600

    last commit

---
+++ module_21_practice_tasks.js
@@ -0,0 +1,74 @@
+// task 1 : Take four parameters. Multiply the four numbers and then return the result
+
+// function parameters (num1, num2, num3, num4){
+//     const result = num1 * num2 * num3 * num4;
+//     return result;
+// }
+// parameters(2, 4, 6, 8);
+
+// splice() syntex 
+// let numbers = [2, 3, 5, 8, 9 ,10];
+// numbers.splice(1,2,15,30);
+// console.log(numbers);
+
+// task 2 : Take a number if the number is odd multiply it by 2 and return the result. If the number is even divide it by two and return the result.
+
+// function numbers(number){
+//     if(number % 2 === 1){
+//         const result = number * 2;
+//         return result;
+//     }
+//     else{
+//         const result = number / 2;
+//         return result;
+//     }
+// }
+// const finalResult = numbers(14);
+// console.log(finalResult);
+
+// system 2
+
+// function numbers(num){
+//     return num % 2 === 1 ? num * 2 : num / 2;
+// }
+
+// const finalResult = numbers(14);
+// console.log(finalResult);
+
+
+// task 3 : Write a function called make_avg() which will take an array of integers and the size of that array and return the average of those values.
+// function make_avg(numbers, size){
+//     let sum = 0;
+//     for(let num of numbers){
+//         sum = sum + num;
+//     }
+//     const divide = sum / size;
+//     return divide;
+
+// }
+
+// const arrayNumbers = [2, 5, 6, 8, 10];
+// const result = make_avg(arrayNumbers, arrayNumbers.length);
+// console.log(result);
+// console.log(parseInt(result));
+
+// task 4 : Write a function called count_zero() which will take a binary string (Binary string is a string which is consist of only 0 and 1) as parameter and count how many 0’s are there in that string.
+
+// 
+
+// task 5 : Write a function called odd_even() which takes an integer value and tells whether this value is even or odd. If even return Even. If odd return Odd
+
+function odd_even(number) {
+    if(number % 2 === 0){
+        return 'Even';
+    }
+    else{
+        return 'Odd';
+    }
+}
+
+const result = odd_even(12);
+console.log(result);
+
+
+
